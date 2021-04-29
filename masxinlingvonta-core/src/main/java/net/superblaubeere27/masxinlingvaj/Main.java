5dce7615cb3af067378c7f3f5c03fc3d1d1254c9
superblaubeere27 <arschwixxa123@gmail.com>
2021-04-29 23:14:28 +0200

    Updated version

--- masxinlingvonta-core/src/main/java/net/superblaubeere27/masxinlingvaj/Main.java
+++ masxinlingvonta-core/src/main/java/net/superblaubeere27/masxinlingvaj/Main.java
@@ -1,13 +1,14 @@
 package net.superblaubeere27.masxinlingvaj;
 
 import net.superblaubeere27.masxinlingvaj.compiler.MLVCompiler;
+import net.superblaubeere27.masxinlingvaj.compiler.tree.CompilerClass;
 import net.superblaubeere27.masxinlingvaj.compiler.tree.CompilerMethod;
 import net.superblaubeere27.masxinlingvaj.preprocessor.AbstractPreprocessor;
 import net.superblaubeere27.masxinlingvaj.preprocessor.CompilerPreprocessor;
+import org.objectweb.asm.Opcodes;
 
 import java.io.File;
 
-import static org.bytedeco.llvm.global.LLVM.LLVMDumpModule;
 import static org.bytedeco.llvm.global.LLVM.LLVMPrintModuleToFile;
 
 public class Main {
@@ -17,13 +18,19 @@ public class Main {
                 new AbstractPreprocessor() {
                     @Override
                     public void init(MLVCompiler compiler, CompilerPreprocessor preprocessor) throws Exception {
-//                        preprocessor.markForCompilation(compiler.getIndex().getMethod("Frame", "mouseMoved", "(Ljava/awt/event/MouseEvent;)V"));
-//                        preprocessor.markForCompilation(compiler.getIndex().getMethod("Frame", "actionPerformed", "(Ljava/awt/event/ActionEvent;)V"));
-//                        preprocessor.markForCompilation(compiler.getIndex().getMethod("Frame", "paintComponent", "(Ljava/awt/Graphics;)V"));
-//                        preprocessor.markForCompilation(compiler.getIndex().getMethod("APathfinding", "findPath", "(LNode;)V"));
-                        preprocessor.markForCompilation(compiler.getIndex().getMethod("Test",
-                                "test",
-                                "()Ljava/util/concurrent/Callable;"));
+                        for (CompilerClass aClass : compiler.getIndex().getClasses()) {
+                            if (!aClass.getClassNode().name.startsWith("net/ccbluex/liquidbounce/"))
+                                continue;
+
+                            for (CompilerMethod method : aClass.getMethods()) {
+                                if ((method.getNode().access & (Opcodes.ACC_NATIVE | Opcodes.ACC_ABSTRACT)) != 0)
+                                    continue;
+                                if (method.getNode().name.startsWith("<"))
+                                    continue;
+
+                                preprocessor.markForCompilation(method);
+                            }
+                        }
                     }
 
                     @Override
@@ -35,16 +42,22 @@ public class Main {
 //                new AnnotationPreprocessor()
         ));
 
-        mlv.loadInput(new File("testJars/Test.jar"));
+        mlv.loadInput(new File("testJars/liquidbounce.jar"));
 
         mlv.preprocessAndCompile();
 
-        mlv.writeOutput(new File("testJars/Test-obf.jar"));
+        mlv.writeOutput(new File("testJars/liquidbounce-obf.jar"));
+
+        LLVMPrintModuleToFile(mlv.getLLVMModule(), "testJars/lb-native.ll", new byte[0]);
 
         mlv.optimize(3);
 
-        LLVMPrintModuleToFile(mlv.getLLVMModule(), "testJars/test-native.ll", new byte[0]);
-        LLVMDumpModule(mlv.getLLVMModule());
+        LLVMPrintModuleToFile(mlv.getLLVMModule(), "testJars/lb-native.ll", new byte[0]);
+//        LLVMDumpModule(mlv.getLLVMModule());
+    }
+
+    private static Runnable lambda() {
+        return () -> System.out.println("Helo");
     }
 
 }
